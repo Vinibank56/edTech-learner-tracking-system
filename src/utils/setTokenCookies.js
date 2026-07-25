432800b14c858c4e9198f983d4180d6442973120
vincent Ebube <onuegbunamvincent@gmail.com>
2026-07-25 23:48:52 +0100

    fix: modified the setTokenCookies so that sameSite:none for different origin connection in production

--- src/utils/setTokenCookies.js
+++ src/utils/setTokenCookies.js
@@ -3,14 +3,14 @@ const jwt = require('jsonwebtoken');
 const setTokenCookies= (res, tokens) => {
   res.cookie('accessToken', tokens.accessToken, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',
-    sameSite: 'strict',
+    secure: true,
+    sameSite: 'none',
     maxAge: 15 * 60 * 1000
   });
   res.cookie('refreshToken', tokens.refreshToken, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',
-    sameSite: 'strict',
+    secure: true,
+    sameSite: 'none',
     maxAge: 7 * 24 * 60 * 60 * 1000
   });
 };
